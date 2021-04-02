59500bc0ab3fd68a393a4e3bdce5dd2418b9f2ff
mh-anwar <mohammad.anwar2939@gmail.com>
2021-04-02 01:09:56 -0400

    Added form, and basic functions

--- popup_Script.js
+++ popup_Script.js
@@ -1,10 +1,22 @@
 //This is the copy to clipboard function
-var clipboard = new ClipboardJS('.btn');
+var clipboard = new ClipboardJS(".btn");
 
-clipboard.on('success', function (e) {
+clipboard.on("success", function (e) {
   console.log(e);
 });
 
-clipboard.on('error', function (e) {
+clipboard.on("error", function (e) {
   console.log(e);
+});
+
+document.getElementById("target").addEventListener("change", function () {
+  "use strict";
+  var vis = document.querySelector(".vis"),
+    target = document.getElementById(this.value);
+  if (vis !== null) {
+    vis.className = "inv";
+  }
+  if (target !== null) {
+    target.className = "vis";
+  }
 });
