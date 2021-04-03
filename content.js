5dc68a45105fa116586af54dbbe81ab58f0a5b16
mh-anwar <mohammad.anwar2939@gmail.com>
2021-04-03 16:25:31 -0400

    Added basic HTML injection, options page Also added margins for buttons

--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-console.log("this works");
+console.log("Content Script is running, SIR!");
+
+var div = document.createElement("div");
+
+document.body.appendChild(div);
+
+div.innerText = "<h1>WGAT NANDATO!<h1>";
