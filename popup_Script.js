69880138fdce98d4d58977152d5194726c6519b4
mh-anwar <mohammad.anwar2939@gmail.com>
2021-04-08 18:08:48 -0400

    New copy to clipboard method added

--- popup_Script.js
+++ popup_Script.js
@@ -1,26 +1,17 @@
-//This is the copy to clipboard function
-console.log("POPUP SCRIPT RUNNING SIR!");
-var clipboard = new ClipboardJS(".btn");
-
-clipboard.on("success", function (e) {
-  console.log(e);
-});
-
-clipboard.on("error", function (e) {
-  console.log(e);
-});
 //Below is the code for the buttons within the dropdowns
-document.getElementById("target").addEventListener("change", function () {
-  "use strict";
-  var vis = document.querySelector(".vis"),
-    target = document.getElementById(this.value);
-  if (vis !== null) {
-    vis.className = "inv";
-  }
-  if (target !== null) {
-    target.className = "vis";
-  }
-});
+document
+  .getElementById("selection_Form")
+  .addEventListener("change", function () {
+    "use strict";
+    var vis = document.querySelector(".vis"),
+      target = document.getElementById(this.value);
+    if (vis !== null) {
+      vis.className = "inv";
+    }
+    if (target !== null) {
+      target.className = "vis";
+    }
+  });
 
 //Options page opener
 document.querySelector("#go-to-options").addEventListener("click", function () {
@@ -30,5 +21,16 @@ document.querySelector("#go-to-options").addEventListener("click", function () {
     window.open(chrome.runtime.getURL("settings_Basic.html"));
   }
 });
+
+//The Copy Function
+document.getElementById("copy_Button").addEventListener("click", function () {
+  const the_Id = document.getElementById("copy_Button");
+  const to_Copy = the_Id.getAttribute("name");
+  navigator.clipboard.writeText(to_Copy);
+});
+
+/* .then( function () { console.log("YES"); }, function () { 
+  alert("Hmm..There seems to be an issue on your end.");
+ } ); */
 
 //Come to the dark side, we use cookies!
