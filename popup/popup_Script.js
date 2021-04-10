f7ccdd94c4daa86517150713bbf517498b95623e
mh-anwar <mohammad.anwar2939@gmail.com>
2021-04-09 23:55:04 -0400

    Added new changes to files

---
+++ popup/popup_Script.js
@@ -0,0 +1,43 @@
+//Below is the code for the buttons within the dropdowns
+document
+  .getElementById("selection_Form")
+  .addEventListener("change", function () {
+    "use strict";
+    var vis = document.querySelector(".vis"),
+      target = document.getElementById(this.value);
+    if (vis !== null) {
+      vis.className = "inv";
+    }
+    if (target !== null) {
+      target.className = "vis";
+    }
+  });
+
+//Options page opener
+document.querySelector("#go-to-options").addEventListener("click", function () {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL("settings_Basic.html"));
+  }
+});
+
+//The Copy Function
+var classes = document.getElementsByClassName("copyButton");
+console.log(classes.length)
+console.log(classes[0])
+for (var i = 0; i < classes.length; i++) {
+  classes[i].addEventListener("click", function (e) {
+    console.log("yaa");
+    console.log(e);
+    console.log(e.target.id);
+    var to_Copy = e.target.id;
+    navigator.clipboard.writeText(to_Copy);
+  });
+}
+
+/* .then( function () { console.log("YES"); }, function () {
+  alert("Hmm..There seems to be an issue on your end.");
+ } ); */
+
+//Come to the dark side, we use cookies!
