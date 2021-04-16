74d1be31416a6c7f8a7b92b0d1292a3ec3bcd781
mh-anwar <mohammad.anwar2939@gmail.com>
2021-04-16 02:19:24 -0400

    Added toolbar, flex container

--- worker.js
+++ worker.js
@@ -1,1 +1,5 @@
 console.log("Service Worker (a.k.a Background Script) is running SIR!");
+
+chrome.runtime.onInstalled.addListener(() => {
+    console.log('Add greeting page with tutorial here');
+});
