1a1b6c27c734d439311503a61e22f5a7c068af96
mh-anwar <mohammad.anwar2939@gmail.com>
2021-08-20 15:51:25 -0400

    Idk

--- content.js
+++ content.js
@@ -1,28 +1,28 @@
-console.log("Content Script is running, SIR!");
+console.log('Content Script is running, SIR!');
 //Apr 05, 09:41pm [YIP]: the content script is sandboxed to within the screen
 var state_of_toolbar = 0;
-let toolbar_height = "20%";
+let toolbar_height = '20%';
 
 chrome.storage.onChanged.addListener((changes, area) => {
   if (changes.toolbar_height?.newValue) {
     console.log(changes.toolbar_height?.newValue);
-    toolbar_height = changes.toolbar_height?.newValue + "%";
-    var toolbar = document.getElementById("copy_toolbar");
-    if (typeof toolbar != "undefined" && toolbar != null) {
-      document.getElementById("copy_toolbar").style.height = toolbar_height;
+    toolbar_height = changes.toolbar_height?.newValue + '%';
+    var toolbar = document.getElementById('copy_toolbar');
+    if (typeof toolbar != 'undefined' && toolbar != null) {
+      document.getElementById('copy_toolbar').style.height = toolbar_height;
     }
   }
 });
 //injection function
 function toolbar_inserter() {
   var div = document.createElement(div);
-  div.className = "toolbar_base";
-  div.id = "copy_toolbar";
+  div.className = 'toolbar_base';
+  div.id = 'copy_toolbar';
   document.body.appendChild(div);
-  fetch(chrome.runtime.getURL("toolbar/content_injection.html"))
+  fetch(chrome.runtime.getURL('toolbar/content_injection.html'))
     .then((response) => response.text())
     .then((data) => {
-      document.getElementById("copy_toolbar").innerHTML = data;
+      document.getElementById('copy_toolbar').innerHTML = data;
       //Call the function that populates the select form
       toolbar_select_form_populator();
       //Call the function that allows buttons to copy
@@ -31,30 +31,30 @@ function toolbar_inserter() {
       toolbar_closer();
       //Call the toolbar minimizer function
       toolbar_minimizer();
-      chrome.storage.sync.get("toolbar_height", function (data) {
-        toolbar_height = data.toolbar_height + "%";
-        document.getElementById("copy_toolbar").style.height = toolbar_height;
+      chrome.storage.sync.get('toolbar_height', function (data) {
+        toolbar_height = data.toolbar_height + '%';
+        document.getElementById('copy_toolbar').style.height = toolbar_height;
       });
     });
 }
 function toolbar_closer() {
   document
-    .getElementById("close_toolbar")
-    .addEventListener("click", function () {
-      chrome.storage.sync.get("toolbar_state", function (data) {
+    .getElementById('close_toolbar')
+    .addEventListener('click', function () {
+      chrome.storage.sync.get('toolbar_state', function (data) {
         chrome.storage.sync.set({ toolbar_state: (data.toolbar_state += 1) });
       });
-      document.getElementById("copy_toolbar").remove();
+      document.getElementById('copy_toolbar').remove();
       state_of_toolbar = 0;
     });
 }
 
 function toolbar_copier() {
   var copyButtonClass = document.getElementsByClassName(
-    "toolbar__button--copy"
+    'toolbar__button--copy'
   );
   for (var i = 0; i < copyButtonClass.length; i++) {
-    copyButtonClass[i].addEventListener("click", function (e) {
+    copyButtonClass[i].addEventListener('click', function (e) {
       var text_to_copy = e.target.textContent;
       navigator.clipboard.writeText(text_to_copy);
     });
@@ -63,46 +63,46 @@ function toolbar_copier() {
 
 function toolbar_select_form_populator() {
   document
-    .getElementById("toolbar__select-form")
-    .addEventListener("change", function () {
-      var buttons_toggle = document.querySelector(".buttons_toggle"),
+    .getElementById('toolbar__select-form')
+    .addEventListener('change', function () {
+      var buttons_toggle = document.querySelector('.buttons_toggle'),
         target = document.getElementById(this.value);
       if (buttons_toggle !== null) {
-        buttons_toggle.className = "toolbar__button-list--div";
+        buttons_toggle.className = 'toolbar__button-list--div';
       }
       if (target !== null) {
-        target.className = "buttons_toggle";
+        target.className = 'buttons_toggle';
       }
     });
 }
 function toolbar_maximizer() {
-  var maximize_toolbar = document.getElementById("maximize_toolbar");
-  maximize_toolbar.addEventListener("click", function () {
-    document.getElementById("minimized_toolbar").remove();
-    document.getElementById("copy_toolbar").style.display = "block";
+  var maximize_toolbar = document.getElementById('maximize_toolbar');
+  maximize_toolbar.addEventListener('click', function () {
+    document.getElementById('minimized_toolbar').remove();
+    document.getElementById('copy_toolbar').style.display = 'block';
   });
 }
 function toolbar_minimized_remover() {
   document
-    .getElementById("close_minmized_toolbar")
-    .addEventListener("click", function () {
-      document.getElementById("minimized_toolbar").remove();
-      document.getElementById("copy_toolbar").remove();
+    .getElementById('close_minmized_toolbar')
+    .addEventListener('click', function () {
+      document.getElementById('minimized_toolbar').remove();
+      document.getElementById('copy_toolbar').remove();
     });
 }
 function toolbar_minimizer() {
   document
-    .getElementById("minimize_toolbar")
-    .addEventListener("click", function () {
+    .getElementById('minimize_toolbar')
+    .addEventListener('click', function () {
       var div = document.createElement(div);
-      div.className = "toolbar_base--minimized";
-      div.id = "minimized_toolbar";
+      div.className = 'toolbar_base--minimized';
+      div.id = 'minimized_toolbar';
       document.body.appendChild(div);
-      document.getElementById("copy_toolbar").style.display = "none";
-      fetch(chrome.runtime.getURL("toolbar/minimize_injection.html"))
+      document.getElementById('copy_toolbar').style.display = 'none';
+      fetch(chrome.runtime.getURL('toolbar/minimize_injection.html'))
         .then((response) => response.text())
         .then((data) => {
-          document.getElementById("minimized_toolbar").innerHTML = data;
+          document.getElementById('minimized_toolbar').innerHTML = data;
           toolbar_maximizer();
           toolbar_minimized_remover();
         });
@@ -111,9 +111,9 @@ function toolbar_minimizer() {
 }
 
 function message_parser(message, sender, sendResponse) {
-  var toolbar = document.getElementById("copy_toolbar");
-  var minimized_toolbar = document.getElementById("minimized_toolbar");
-  if (message === "state_of_toolbar") {
+  var toolbar = document.getElementById('copy_toolbar');
+  var minimized_toolbar = document.getElementById('minimized_toolbar');
+  if (message === 'state_of_toolbar') {
     if (document.contains(toolbar)) {
       sendResponse({ state: 1 });
     } else if (document.contains(minimized_toolbar)) {
@@ -121,16 +121,16 @@ function message_parser(message, sender, sendResponse) {
     } else {
       sendResponse({ state: 0 });
     }
-  } else if (message === "toolbar_on") {
+  } else if (message === 'toolbar_on') {
     //Call the function that inserts the toolbar
     toolbar_inserter();
     state_of_toolbar = 1;
-  } else if (message === "toolbar_off") {
+  } else if (message === 'toolbar_off') {
     if (document.contains(toolbar)) {
-      document.getElementById("copy_toolbar").remove();
+      document.getElementById('copy_toolbar').remove();
     }
     if (document.contains(minimized_toolbar)) {
-      document.getElementById("minimized_toolbar").remove();
+      document.getElementById('minimized_toolbar').remove();
     }
     state_of_toolbar = 0;
   }
--- popup/popup_script.js
+++ popup/popup_script.js
@@ -8,50 +8,58 @@ Use single quotes (instead of double quotes) where necessary
 */
 let toolbar = document.getElementById('open_toolbar');
 let select_form = document.getElementById('select_form');
-//Checks the state of the toolbar and changes the text content of the Toolbar Button
-window.onload = function () {
-  chrome.tabs.query({ active: true, currentWindow: true }, msger);
-  function msger(tabs) {
-    let content_script_message = 'state_of_toolbar';
-    chrome.tabs.sendMessage(tabs[0].id, content_script_message, function (response) {
-      if (typeof (response) != 'undefined' && response != null) {
-        if (response.state === 1) {
-          toolbar.textContent = 'Close Toolbar';
-        }
-        if (response.state === 0) {
-          toolbar.textContent = 'Open Toolbar (CTRL+Q)';
-        }
-      }
-    });
-  }
-};
-//Code to send message from popup to content to open toolbar #WIP
-toolbar.addEventListener('click', toolbar_state_receiver);
-function toolbar_state_receiver() {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    let message = "state_of_toolbar"
-    //Ask to check if the toolbar is opened or closed
-    chrome.tabs.sendMessage(tabs[0].id, message, function (response) {
-      //Calls the function that opens or closes the toolbar
-      toolbar_opener(response.state, tabs);
-    });
-  });
-};
+let options_button = document.getElementById('options_button');
+let copyButtonClass = document.getElementsByClassName('button--copy');
 
-function toolbar_opener(state, tabs) {
-  if (state === 0) {
-    let content_script_message = 'toolbar_on';
-    chrome.tabs.sendMessage(tabs[0].id, content_script_message);
+//Sends message to the tab then calls a function to run
+async function content_script_messenger(function_to_run) {
+  let tabs_info = await get_current_tab();
+  console.log(tabs_info);
+  let content_script_message = 'state_of_toolbar';
+  chrome.tabs.sendMessage(
+    tabs_info.id,
+    content_script_message,
+    function (response) {
+      function_to_run(response, tabs_info);
+    }
+  );
+}
+//Gets tab data
+async function get_current_tab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
+
+//Checks the state of the toolbar and changes the text content of the Toolbar Button
+function toolbar_button_textcontent(response, tabs_info) {
+  if (typeof response != 'undefined' && response != null) {
+    if (response.state === 1) {
+      toolbar.textContent = 'Close Toolbar';
+    }
+    if (response.state === 0) {
+      toolbar.textContent = 'Open Toolbar (CTRL+Q)';
+    }
+  }
+}
+
+//Code to send message from popup to content to open toolbar
+function toolbar_opener(response, tabs_info) {
+  let content_script_message;
+  if (response.state === 0) {
+    content_script_message = 'toolbar_on';
+    chrome.tabs.sendMessage(tabs_info.id, content_script_message);
     toolbar.textContent = 'Close Toolbar';
-  } else if (state === 1) {
-    let content_script_message = 'toolbar_off';
-    chrome.tabs.sendMessage(tabs[0].id, content_script_message);
+  } else if (response.state === 1) {
+    content_script_message = 'toolbar_off';
+    chrome.tabs.sendMessage(tabs_info.id, content_script_message);
     toolbar.textContent = 'Open Toolbar (CTRL+Q)';
   }
 }
+
 //Code for the dropdown to show the copy buttons
-select_form.addEventListener('change', function () {
-  var buttons_toggle = document.querySelector('.buttons_toggle'),
+function show_copy_buttons() {
+  let buttons_toggle = document.querySelector('.buttons_toggle'),
     target = document.getElementById(this.value);
   if (buttons_toggle !== null) {
     buttons_toggle.className = 'button__list--div';
@@ -59,23 +67,41 @@ select_form.addEventListener('change', function () {
   if (target !== null) {
     target.className = 'buttons_toggle';
   }
-});
+}
 
-//Code to open the options page (through button in popup)
-document.querySelector('#options_button').addEventListener('click', function () {
+function open_options() {
+  //Code to open the options page (through button in popup)
   if (chrome.runtime.openOptionsPage) {
     chrome.runtime.openOptionsPage();
   } else {
     window.open(chrome.runtime.getURL('../settingsPage/settingsBasic.html'));
   }
-});
-
-//Code for the Copy Function (for the copy buttons)
-var copyButtonClass = document.getElementsByClassName('button--copy');
-
-for (var i = 0; i < copyButtonClass.length; i++) {
-  copyButtonClass[i].addEventListener('click', function (e) {
-    var text_to_copy = e.target.textContent;
-    navigator.clipboard.writeText(text_to_copy);
-  });
 }
+
+function attach_copy_buttons() {
+  //Code for the Copy Function (for the copy buttons)
+  for (let i = 0; i < copyButtonClass.length; i++) {
+    copyButtonClass[i].addEventListener('click', function (event) {
+      let text_to_copy = event.target.textContent;
+      navigator.clipboard.writeText(text_to_copy);
+    });
+  }
+}
+
+function execute_popup_funcs() {
+  content_script_messenger(toolbar_button_textcontent);
+  select_form.addEventListener('change', show_copy_buttons);
+  attach_copy_buttons();
+  toolbar.addEventListener('click', () =>
+    content_script_messenger(toolbar_opener)
+  );
+  options_button.addEventListener('click', open_options);
+}
+
+document.addEventListener('readystatechange', (event) => {
+  if (event.target.readyState === 'interactive') {
+    execute_popup_funcs();
+  } else if (event.target.readyState === 'complete') {
+    execute_popup_funcs();
+  }
+});
